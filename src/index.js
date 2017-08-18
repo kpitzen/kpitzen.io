454971cd9225e2af6f86a68b396a9fa49e5a1714
Kyle Pitzen <kyle.pitzen@gmail.com>
2017-08-17 22:59:28 -0500

    have automated builds working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import './index.css';
 
 // var testData = [1, 2, 3, 4, 1, 2];
